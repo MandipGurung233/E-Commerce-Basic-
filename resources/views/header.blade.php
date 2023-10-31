0230c563fea5298543be43f60948b7221477168c
Mandip Gurung <85865218+MandipGurung233@users.noreply.github.com>
2023-10-31 13:10:46 +0545

    Name Change

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -16,7 +16,7 @@ if(Session::has('user'))
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="/">E-Cloth</a>
+        <a class="navbar-brand" href="/">Kinmel</a>
       </div>
   
       <!-- Collect the nav links, forms, and other content for toggling -->
